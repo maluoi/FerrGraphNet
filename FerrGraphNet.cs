48680e47c746da2f1a40f8e7d1c443907d9ca3ae
Nick Klingensmith <programmerpichu@gmail.com>
2019-12-01 10:47:47 -0800

    -C#, Added iterators, enumerables, some algorithms, general quality of life improvements :)

--- FerrGraphNet.cs
+++ FerrGraphNet.cs
@@ -6,6 +6,19 @@ using System.Text;
 
 namespace FerrGraphNet
 {
+	public enum NodeLoc
+	{
+		In,
+		Out,
+		Any
+	}
+
+	interface IGraphLookup<N,E>
+	{
+		public Node<N,E> GetNode(int index);
+		public Edge<E> GetEdge(int index);
+	}
+
     public class GraphLibrary<T,G,N,E>
     {
         enum Active {
@@ -126,11 +139,18 @@ namespace FerrGraphNet
         }
     }
 
-    public class Graph<T,N,E>
+    public class Graph<T,N,E> : IGraphLookup<N,E>
     {
-        internal List<Node<N>> nodes = new List<Node<N>>();
-        internal List<Edge<E>> edges = new List<Edge<E>>();
-        public   GraphData<T>  data  = new GraphData<T>();
+        internal List<Node<N,E>> nodes = new List<Node<N,E>>();
+        internal List<Edge<E>>   edges = new List<Edge<E>>();
+        public   GraphData<T>    data  = new GraphData<T>();
+
+        public IEnumerable<Node<N,E>> Nodes { get => nodes; }
+        public IEnumerable<Edge<E>>   Edges { get => edges; }
+		public int NodeCount { get => nodes.Count; }
+        public int EdgeCount { get => edges.Count; }
+        public Node<N,E> GetNode(int index) => nodes[index];
+        public Edge<E>   GetEdge(int index) => edges[index];
 
         public string Id { get; private set;}
 
@@ -139,31 +159,82 @@ namespace FerrGraphNet
             Id = id;
         }
 
-        public Node<N> AddNode(string id)
-            => nodes[AddNodeId(id)];
-        public int AddNodeId(string id)
+		public Node<N,E> AddNode(string id, N data = default(N))
+            => nodes[AddNodeId(id, data)];
+        public int AddNodeId(string id, N data = default(N))
         {
-            nodes.Add(new Node<N>(id));
+            nodes.Add(new Node<N,E>(this, id));
+            nodes[nodes.Count-1].data.parsed = data;
             return nodes.Count - 1;
         }
         
         public int FindNode(string id)
             => nodes.FindIndex(n=>n.Id == id);
 
-        public Edge<E> AddEdge(Node<N> start, Node<N> end)
-            => edges[AddEdgeId(FindNode(start.Id), FindNode(end.Id))];
-        public Edge<E> AddEdge(string startId, string endId)
-            => edges[AddEdgeId(FindNode(startId), FindNode(endId))];
-        public Edge<E> AddEdge(int start, int end)
+        public Edge<E> AddEdge(Node<N,E> start, Node<N,E> end, E data = default(E))
+            => edges[AddEdgeId(FindNode(start.Id), FindNode(end.Id), data)];
+        public Edge<E> AddEdge(string startId, string endId, E data = default(E))
+            => edges[AddEdgeId(FindNode(startId), FindNode(endId), data)];
+        public Edge<E> AddEdge(int start, int end, E data = default(E))
             => edges[AddEdgeId(start, end)];
-        public int AddEdgeId(string startId, string endId)
-            => AddEdgeId(FindNode(startId), FindNode(endId));
-        public int AddEdgeId(int start, int end)
+        public int AddEdgeId(string startId, string endId, E data = default(E))
+            => AddEdgeId(FindNode(startId), FindNode(endId), data);
+        public int AddEdgeId(int start, int end, E data = default(E))
         {
             edges.Add(new Edge<E>(start, end));
+            edges[edges.Count - 1].data.parsed = data;
             nodes[start].outEdges.Add(edges.Count - 1);
             nodes[end].inEdges.Add(edges.Count - 1);
             return edges.Count - 1;
+        }
+
+
+        /// <summary>Travels up the tree, and find all the roots (nodes with no inputs) attached
+        /// to the provided nodes.</summary>
+        /// <param name="ofNodeIds">Ids of the nodes you want to find the roots of.</param>
+        /// <returns>A list of node ids containing all the roots (nodes with no inputs) that are 
+        /// connected to the provided list of nodes.</returns>
+        public List<int> FindRoots(List<int> ofNodeIds)
+        {
+            List<int> curr = new List<int>(ofNodeIds);
+            List<int> result = new List<int>();
+            while (curr.Count > 0)
+            {
+                int id = curr[curr.Count - 1];
+                var n  = nodes[id];
+                curr.RemoveAt(curr.Count-1);
+
+                if (n.inEdges.Count == 0)
+                    result.Add(id);
+                else
+                    for (int i = 0; i < n.inEdges.Count; i++) { 
+						var e = edges[n.inEdges[i]];
+                        if (!curr.Contains(e.Start))
+                            curr.Add(e.Start);
+					}
+            }
+            return result;
+        }
+
+        public List<int> FindConnected(List<int> toNodeIds)
+        {
+            List<int> search = new List<int>(toNodeIds);
+
+            for (int index = 0; index < search.Count; index++)
+            {
+                var n = nodes[search[index]];
+                for (int i = 0; i < n.inEdges.Count; i++) { 
+					var e = edges[n.inEdges[i]];
+                    if (!search.Contains(e.Start))
+                        search.Add(e.Start);
+				}
+                for (int i = 0; i < n.outEdges.Count; i++) {
+					var e = edges[n.outEdges[i]];
+					if (!search.Contains(e.End))
+                        search.Add(e.End);
+				}
+            }
+            return search;
         }
 
         public string Save()
@@ -190,25 +261,59 @@ namespace FerrGraphNet
         }
     }
 
-    public class Node<T>
+    public class Node<N, E>
     {
+		#region Fields and Properties
+
+		IGraphLookup<N,E> graphLookup;
+
         internal List<int>    inEdges;
         internal List<int>    outEdges;
-        public   GraphData<T> data;
+        public   GraphData<N> data;
 
-        public float x,y,z;
+        public float x, y, z;
 
         public string Id { get; private set; }
 
-        public Node(string id) {
+		#endregion
+
+        internal Node(IGraphLookup<N, E> graphLookup, string id) {
+			this.graphLookup = graphLookup;
             inEdges  = new List<int>();
             outEdges = new List<int>();
-            data     = new GraphData<T>();
+            data     = new GraphData<N>();
             x=y=z=0;
             Id = id;
         }
 
-        internal void Save(StringBuilder result)
+		#region Enumeration/Iteration
+
+		public IEnumerable<Edge<E>> Edges(NodeLoc loc) { switch(loc) { 
+			case NodeLoc.In:  for (int i = 0; i < inEdges.Count;  i++) yield return graphLookup.GetEdge(inEdges[i]); break;
+			case NodeLoc.Out: for (int i = 0; i < outEdges.Count; i++) yield return graphLookup.GetEdge(outEdges[i]); break;
+			case NodeLoc.Any: 
+					for (int i = 0; i < inEdges.Count;  i++) yield return graphLookup.GetEdge(inEdges[i]);
+					for (int i = 0; i < outEdges.Count; i++) yield return graphLookup.GetEdge(outEdges[i]); break;
+		} }
+		public IEnumerable<Node<N,E>> Nodes(NodeLoc loc) { switch(loc) { 
+			case NodeLoc.In:  for (int i = 0; i < inEdges.Count;  i++) yield return graphLookup.GetNode(graphLookup.GetEdge(inEdges [i]).Start); break;
+			case NodeLoc.Out: for (int i = 0; i < outEdges.Count; i++) yield return graphLookup.GetNode(graphLookup.GetEdge(outEdges[i]).End  ); break;
+			case NodeLoc.Any:
+				for (int i = 0; i < inEdges.Count;  i++) yield return graphLookup.GetNode(graphLookup.GetEdge(inEdges [i]).Start);
+				for (int i = 0; i < outEdges.Count; i++) yield return graphLookup.GetNode(graphLookup.GetEdge(outEdges[i]).End  ); break;
+		} }
+		public int       OutCount => outEdges.Count;
+		public int       InCount  => inEdges.Count;
+		public Node<N,E> OutNode (int index) => graphLookup.GetNode(graphLookup.GetEdge(outEdges[index]).End);
+		public Node<N,E> InNode  (int index) => graphLookup.GetNode(graphLookup.GetEdge(inEdges[index]).Start);
+		public Edge<E>   InEdge  (int index) => graphLookup.GetEdge(inEdges[index]);
+		public Edge<E>   OutEdge (int index) => graphLookup.GetEdge(outEdges[index]);
+
+		#endregion
+
+		#region String and Saving
+
+		internal void Save(StringBuilder result)
         {
             result.Append("-n ");
             result.Append(Id);
@@ -219,6 +324,8 @@ namespace FerrGraphNet
         {
             return string.Format("Node {0}", Id);
         }
+		
+		#endregion
     }
 
     public class Edge<T>
@@ -226,6 +333,9 @@ namespace FerrGraphNet
         int                 start;
         int                 end;
         public GraphData<T> data;
+
+        public int Start { get=>start; }
+        public int End { get=>end; }
 
         public Edge(int start, int end)
         {
@@ -258,6 +368,7 @@ namespace FerrGraphNet
             get { return pairs[key]; }
             set { pairs[key] = value; }
         }
+        public IEnumerable<KeyValuePair<string, string>> Enumerable { get=>pairs; }
 
         public GraphData<T> Add(string key, string val)
         {
